3031d287450871db68940f3533041faeadffef25
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-13 15:48:04 +0900

    fetch single post

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,50 +1,55 @@
 import Image from "next/image";
 import style from "./singlepage.module.css";
 import Comments from "../../../components/comments/Comments";
-const SinglePage = () => {
+
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+  const data = await getData(slug);
   return (
     <div className={style.container}>
       <div className={style.infoContainer}>
         <div className={style.textContainer}>
-          <h1 className={style.title}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          </h1>
+          <h1 className={style.title}>{data?.title}</h1>
           <div className={style.user}>
             <div className={style.userImageContainer}>
-              <Image src="/photo.jpeg" alt="" fill className={style.avatar} />
+              {data?.user?.image && (
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={style.avatar}
+                />
+              )}
             </div>
             <div className={style.userTextContainer}>
-              <span className={style.username}>John</span>
+              <span className={style.username}>{data?.user.name}</span>
               <span className={style.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-        <div className={style.imageContainer}>
-          <Image src="/photo.jpeg" alt="" fill className={style.image} />
-        </div>
+        {data?.img && (
+          <div className={style.imageContainer}>
+            <Image src={data.img} alt="" fill className={style.image} />
+          </div>
+        )}
       </div>
       <div className={style.content}>
         <div className={style.post}>
-          <div className={style.description}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-              reiciendis dolor perspiciatis distinctio, voluptas ad. Amet, alias
-              aliquid? Laudantium, sint. Ipsum libero eius consequatur quod,
-              magnam consectetur ipsam quisquam dignissimos!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-              reiciendis dolor perspiciatis distinctio, voluptas ad. Amet, alias
-              aliquid? Laudantium, sint. Ipsum libero eius consequatur quod,
-              magnam consectetur ipsam quisquam dignissimos!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-              reiciendis dolor perspiciatis distinctio, voluptas ad. Amet, alias
-              aliquid? Laudantium, sint. Ipsum libero eius consequatur quod,
-              magnam consectetur ipsam quisquam dignissimos!
-            </p>
-          </div>
+          <div
+            className={style.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+
           <div className={style.comment}>
             <Comments />
           </div>
