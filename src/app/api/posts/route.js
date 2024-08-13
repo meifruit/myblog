0b757a466d40b98fde43de44c475e02033489297
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-13 14:43:12 +0900

    show the post page by category

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,16 +4,22 @@ import { NextResponse } from "next/server";
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
   const page = searchParams.get("page");
+  const category = searchParams.get("category");
+
   const POST_PER_PAGE = 2;
+
   const safePage = Math.max(page, 1);
   const query = {
     take: POST_PER_PAGE,
     skip: POST_PER_PAGE * (safePage - 1),
+    where: {
+      ...(category && { catSlug: category }),
+    },
   };
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query),
-      prisma.post.count(),
+      prisma.post.count({ where: query.where }),
     ]);
     console.log(posts);
     return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
