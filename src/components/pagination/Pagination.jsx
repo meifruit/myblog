3d59bc29911ced329fe6227fbe412799d6ee1473
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-13 13:37:06 +0900

    add fetch the posts and pagenation

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,12 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
 import style from "./pagination.module.css";
 import { EastOutlined, WestOutlined } from "@mui/icons-material";
-const Pagination = () => {
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
   return (
     <div className={style.container}>
-      <button className={style.buttonleft}>
+      <button
+        className={style.buttonleft}
+        disabled={!hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
         <WestOutlined />
       </button>
-      <button className={style.buttonright}>
+      <button
+        className={style.buttonright}
+        disabled={!hasNext}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
         <EastOutlined />
       </button>
     </div>
