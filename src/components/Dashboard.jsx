2a66edce9172abc5384f8d0acb6bfb33a26541af
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-21 16:29:52 +0900

    edit responsive card and changed the font

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,8 +9,14 @@ const Dashboard = () => {
           href={"/"}
           className="flex gap-3 items-center justify-center py-2"
         >
-          <Image src={"/home.png"} alt={""} width={20} height={20} />
-          <span className="hidden lg:block text-sm font-light capitalize">
+          <Image
+            src={"/home.png"}
+            alt={""}
+            width={20}
+            height={20}
+            className="icon"
+          />
+          <span className="hidden lg:block text-sm font-light uppercase">
             home
           </span>
         </Link>
@@ -18,8 +24,14 @@ const Dashboard = () => {
           href={"/blog"}
           className="flex gap-3 items-center justify-center  py-2"
         >
-          <Image src={"/blog.png"} alt={""} width={20} height={20} />
-          <span className="hidden lg:block text-sm font-light capitalize">
+          <Image
+            src={"/blog.png"}
+            alt={""}
+            width={20}
+            height={20}
+            className="icon"
+          />
+          <span className="hidden lg:block text-sm font-light uppercase">
             blog
           </span>
         </Link>
@@ -27,8 +39,14 @@ const Dashboard = () => {
           href={"/"}
           className="flex gap-3 items-center justify-center py-2"
         >
-          <Image src={"/forward.png"} alt={""} width={20} height={20} />
-          <span className="hidden lg:block text-sm font-light capitalize">
+          <Image
+            src={"/forward.png"}
+            alt={""}
+            width={20}
+            height={20}
+            className="icon"
+          />
+          <span className="hidden lg:block text-sm font-light uppercase">
             video
           </span>
         </Link>
